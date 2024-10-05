1d6ec0eb89e360deac60d7855f4031b161e0c50e
sasquatchdev <carl-neben@gmx.de>
2024-10-05 21:33:30 +0200

    Added NodeJS template

---
+++ NodeJS/Common/README.md
@@ -0,0 +1,2 @@
+## {{ title }} v{{ version }}
+{{ description }}
---
+++ NodeJS/common.lua
@@ -0,0 +1,7 @@
+return {
+  version = forge.prompt_text("Please provide a version"),
+  author = forge.prompt_text("Please provide an author"),
+  description = forge.prompt_text("Please provide a description"),
+  repository = forge.prompt_text("Please provide a repository URL"),
+  license = forge.prompt_text("Please provide a license"),
+}
